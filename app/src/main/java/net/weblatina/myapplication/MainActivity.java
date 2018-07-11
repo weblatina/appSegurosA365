b6cc575a56c50c9ce9d21202a2e112f284e96ee7
francisco <franc.jadev@gmail.com>
2018-07-11 13:46:09 -0400

    uso google vision api

--- app/src/main/java/net/weblatina/myapplication/MainActivity.java
+++ app/src/main/java/net/weblatina/myapplication/MainActivity.java
@@ -13,6 +13,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends Activity implements OnClickListener{
     Button btn;
     ImageView imagen;
@@ -54,11 +73,61 @@ public class MainActivity extends Activity implements OnClickListener{
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
         if (resultCode==Activity.RESULT_OK){
-            Bundle ext = data.getExtras();
-            bmp = (Bitmap)ext.get("data");
-            imagen.setImageBitmap(bmp);
-            imagen.buildDrawingCache();
-            Bitmap bitmap = imagen.getDrawingCache();
+            try {
+                Bundle ext = data.getExtras();
+                bmp = (Bitmap)ext.get("data");
+                imagen.setImageBitmap(bmp);
+                imagen.buildDrawingCache();
+                Bitmap bitmap = imagen.getDrawingCache();
+
+                Vision.Builder visionBuilder = new Vision.Builder(
+                        new NetHttpTransport(),
+                        new AndroidJsonFactory(),
+                        null);
+                visionBuilder.setVisionRequestInitializer(
+                        new VisionRequestInitializer("AIzaSyA5y6vCsWvlrJ45kpF8NC1npaJ0EhCqlTs"));
+                Vision vision = visionBuilder.build();
+
+                Image base64EncodedImage = getBase64EncodedJpeg(bitmap);
+
+                Feature labelDetection = new Feature();
+                labelDetection.setType("LABEL_DETECTION");
+                labelDetection.setMaxResults(1);
+
+                AnnotateImageRequest request = new AnnotateImageRequest();
+                request.setImage(base64EncodedImage);
+                request.setFeatures(Arrays.asList(labelDetection));
+
+                BatchAnnotateImagesRequest batchRequest =
+                        new BatchAnnotateImagesRequest();
+
+                batchRequest.setRequests(Arrays.asList(request));
+
+
+                BatchAnnotateImagesResponse response =
+                        vision.images().annotate(batchRequest).execute();
+
+
+                List<EntityAnnotation> texts = response.getResponses().get(0)
+                        .getTextAnnotations();
+
+                //aquí se muestra la imagen
+                if (texts.get(0).getDescription().contains("car")){
+                    Toast.makeText(this, "En la foto aparece un vehículo", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(this, "En la foto NO aparece un vehículo", Toast.LENGTH_LONG).show();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private Image getBase64EncodedJpeg(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return new Image().encodeContent(byteArray);
     }
 }
